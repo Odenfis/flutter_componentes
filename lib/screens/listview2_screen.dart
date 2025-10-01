f78f860716c92a5f33b85248422199c404549a4c
odenfis <wmoncada@tecsup.edu.pe>
2025-10-01 11:32:22 -0500

    mie1-1132

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ListView2Screen extends StatelessWidget {
 
-  final opciones = const['Korn','Slipknot','Limp Bizkit','System of a Down'];
+  final opciones = const['Korn','Slipknot','Limp Bizkit','System of a Down','Mudvayne'];
    
   const ListView2Screen({super.key});
   
@@ -11,7 +11,20 @@ class ListView2Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('ListView 2 Test'),
-      ),  
+        backgroundColor: Colors.amber,        
+      ),
+      body: ListView.separated(
+        itemBuilder:(context, index) => ListTile(
+          leading: Icon(Icons.music_note),
+          title: Text(opciones[index]),
+          trailing: Icon(Icons.play_arrow, color: Color.fromARGB(100, 218, 140, 6),),
+          onTap: () {
+            final opt = opciones[index];
+            print(opt);
+          },
+        ), 
+        separatorBuilder:(context, index) => Divider(), 
+        itemCount: opciones.length),  
       );
     
   }
